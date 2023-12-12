9075ab1f16cade2e1464f9f0db7dc1306b9f6f76
Shaffat Mahmud <68887458+Shaffat01@users.noreply.github.com>
2023-12-12 14:40:03 +0600

    COMMIT

---
+++ app.py
@@ -0,0 +1,225 @@
+from flask import Flask, flash, request, redirect, render_template
+import os
+from werkzeug.utils import secure_filename
+import cv2
+import pickle
+import imutils
+from tensorflow.keras.models import load_model
+import joblib
+import numpy as np
+from tensorflow.keras.applications.vgg16 import preprocess_input
+
+
+
+
+
+
+covid_model = load_model('models/covid-19_model.h5')
+diabetes_model = pickle.load(open('models/diabetes_model.sav', 'rb'))
+pneumonia_model = load_model('models/pneumonia_model.h5')
+breastcancer_model = joblib.load('models/breast_cancer_model.pkl')
+
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "secret key"
+
+
+
+
+############################################################################################
+
+
+def preprocess_imgs(set_name, img_size):
+
+    set_new = []
+    for img in set_name:
+        img = cv2.resize(img,dsize=img_size,interpolation=cv2.INTER_CUBIC)
+        set_new.append(preprocess_input(img))
+    return np.array(set_new)
+
+
+def crop_imgs(set_name, add_pixels_value=0):
+
+    set_new = []
+    for img in set_name:
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+        thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.erode(thresh, None, iterations=2)
+        thresh = cv2.dilate(thresh, None, iterations=2)
+
+        cnts = cv2.findContours(
+            thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        c = max(cnts, key=cv2.contourArea)
+        
+        extLeft = tuple(c[c[:, :, 0].argmin()][0])
+        extRight = tuple(c[c[:, :, 0].argmax()][0])
+        extTop = tuple(c[c[:, :, 1].argmin()][0])
+        extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+        ADD_PIXELS = add_pixels_value
+        new_img = img[extTop[1]-ADD_PIXELS:extBot[1]+ADD_PIXELS,
+                      extLeft[0]-ADD_PIXELS:extRight[0]+ADD_PIXELS].copy()
+        set_new.append(new_img)
+
+    return np.array(set_new)
+
+##################################################################################################
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def home():
+    return render_template('homepage.html')
+
+
+@app.route('/covid')
+def covid():
+    return render_template('covid.html')
+
+
+@app.route('/breastcancer')
+def breast_cancer():
+    return render_template('breastcancer.html')
+
+
+
+@app.route('/diabetes')
+def diabetes():
+    return render_template('diabetes.html')
+
+
+
+@app.route('/pneumonia')
+def pneumonia():
+    return render_template('pneumonia.html')
+
+
+
+
+###################################################################
+
+
+@app.route('/resultc', methods=['POST'])
+def resultc():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        phone = request.form['phone']
+        gender = request.form['gender']
+        age = request.form['age']
+        file = request.files['file']
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('Image successfully uploaded and displayed below')
+            img = cv2.imread('static/uploads/'+filename)
+            img = cv2.resize(img, (224, 224))
+            img = img.reshape(1, 224, 224, 3)
+            img = img/255.0
+            pred = covid_model.predict(img)
+            if pred < 0.5:
+                pred = 0
+            else:
+                pred = 1
+            blob_image_name = "covid_" + str(pred) + "_" + filename
+            return render_template('resultc.html', filename=filename, fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
+
+        else:
+            flash('Allowed image types are - png, jpg, jpeg')
+            return redirect(request.url)
+
+
+
+
+
+@app.route('/resultd', methods=['POST'])
+def resultd():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        phone = request.form['phone']
+        gender = request.form['gender']
+        pregnancies = request.form['pregnancies']
+        glucose = request.form['glucose']
+        bloodpressure = request.form['bloodpressure']
+        insulin = request.form['insulin']
+        bmi = request.form['bmi']
+        diabetespedigree = request.form['diabetespedigree']
+        age = request.form['age']
+        skinthickness = request.form['skin']
+        
+        print(resultd)
+        
+        
+        pred = diabetes_model.predict(
+            [[pregnancies,glucose,bloodpressure,skinthickness,insulin, bmi, diabetespedigree, age]])
+        return render_template('resultd.html', fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
+
+
+
+@app.route('/resultbc', methods=['POST'])
+def resultbc():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        phone = request.form['phone']
+        gender = request.form['gender']
+        age = request.form['age']
+        cpm = request.form['concave_points_mean']
+        am = request.form['area_mean']
+        rm = request.form['radius_mean']
+        pm = request.form['perimeter_mean']
+        cm = request.form['concavity_mean']
+        pred = breastcancer_model.predict(
+            np.array([cpm, am, rm, pm, cm]).reshape(1, -1))
+        return render_template('resultbc.html', fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
+
+
+
+@app.route('/resultp', methods=['POST'])
+def resultp():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        phone = request.form['phone']
+        gender = request.form['gender']
+        age = request.form['age']
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('Image successfully uploaded and displayed below')
+            img = cv2.imread('static/uploads/'+filename)
+            img = cv2.resize(img, (150, 150))
+            img = img.reshape(1, 150, 150, 3)
+            img = img/255.0
+            pred = pneumonia_model.predict(img)
+            if pred < 0.5:
+                pred = 0
+            else:
+                pred = 1
+            return render_template('resultp.html', filename=filename, fn=firstname, ln=lastname, age=age, r=pred, gender=gender)
+
+        else:
+            flash('Allowed image types are - png, jpg, jpeg')
+            return redirect(request.url)
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
